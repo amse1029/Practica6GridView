2be654f983af381de334cac3bea2070220960551
amse1029 <alexa.soto236348@potros.itson.edu.mx>
2024-03-07 15:17:05 -0700

    Práctica 7 terminada y revisada

--- app/src/main/java/mx/edu/potros/practica6/detalle_pelicula.kt
+++ app/src/main/java/mx/edu/potros/practica6/detalle_pelicula.kt
@@ -1,7 +1,9 @@
 package mx.edu.potros.practica6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -13,12 +15,20 @@ class detalle_pelicula : AppCompatActivity() {
         val iv_pelicula_image:ImageView=findViewById(R.id.iv_pelicula_imagen)
         val tv_nombre_pelicula: TextView =findViewById(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc:TextView=findViewById(R.id.tv_pelicula_desc)
+        val button:Button=findViewById(R.id.buyTickets)
 
         val bundle=intent.extras
+
         if(bundle!=null){
             iv_pelicula_image.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("titulo"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+        }
+
+        button.setOnClickListener {
+
+            var intent:Intent=Intent(this, SeatSelection::class.java)
+            startActivity(intent)
         }
     }
 }
